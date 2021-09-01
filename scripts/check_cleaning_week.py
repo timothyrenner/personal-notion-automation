9d4675cd965d25318eba09e0586ab01840d6f007
Tim Renner <renner.timothy@gmail.com>
2021-09-01 07:41:23 -0500

    Oops should be day not day_of_month.

--- scripts/check_cleaning_week.py
+++ scripts/check_cleaning_week.py
@@ -8,7 +8,7 @@ from loguru import logger
 def main():
     today = pendulum.now("America/Chicago")
     today_days_in_month = today.days_in_month
-    days_left_in_month = today_days_in_month - today.day_of_month
+    days_left_in_month = today_days_in_month - today.day
 
     logger.info(
         f"Days in month for {today.strftime('%Y-%m-%d')}: "
